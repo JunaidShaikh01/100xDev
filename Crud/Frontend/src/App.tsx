4b4ef425daa4b5590fd9593ba48f1bffb7fa4085
JunaidShaikh01 <shaikhjunaid020801@gmail.com>
2025-01-26 12:03:12 +0530

    Creating new Task Manager app

---
+++ Crud/Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="w-full h-screen flex justify-center items-">
+        <h1 className="text-pink font-pink">This is for testing </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
